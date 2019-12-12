5d30ab91a2c8a7be01bd4575f55fd947aa399fa7
Ananye Agarwal <ananayagarwal@gmail.com>
2019-12-12 03:07:47 -0800

    Added a scroll hiding nav bar

--- src/TopNavBar.js
+++ src/TopNavBar.js
@@ -2,16 +2,31 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Slide from '@material-ui/core/Slide';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+
+function HideOnScroll(props) {
+    const { children } = props;
+    const trigger = useScrollTrigger();
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
 
 function NavBar(props) {
     return (
-        <AppBar>
-            <Toolbar>
-                <Typography variant="h5">
-                    TRYST
-                </Typography>
-            </Toolbar>
-        </AppBar>
+        <HideOnScroll>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant="h5">
+                        TRYST
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </HideOnScroll>
     )
 }
 
