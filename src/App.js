c203140148e5731381b6f4b9de8aaf6af83bc522
Ananye Agarwal <ananayagarwal@gmail.com>
2019-12-13 20:36:32 -0800

    Add a simple ImageBanner

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import NavBar from "./TopNavBar";
 import DummyText from "./DummyText";
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import ImageBanner from './ImageBanner';
 
 
 const theme = createMuiTheme({
@@ -10,13 +11,24 @@ const theme = createMuiTheme({
     primary: { main: '#2196F3' },
     secondary: { main: '#4CAF50' }
   }
-})
+});
+
+const mainFeaturedPost = {
+  title: 'Title of a longer featured blog post',
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+  linkText: 'Continue reading…',
+};
+
 
 function App() {
   return (
         <ThemeProvider theme={theme}>
           <React.Fragment>
             <NavBar/>
+            <ImageBanner post={mainFeaturedPost} />
             <DummyText/>
           </React.Fragment>
         </ThemeProvider>
