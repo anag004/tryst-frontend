2296d81fc0613774c466d33f9a468532c610ef47
Ananye Agarwal <ananayagarwal@gmail.com>
2019-12-15 16:38:15 -0800

    Added a Grid to display the event cards

--- src/PageSection.js
+++ src/PageSection.js
@@ -20,21 +20,31 @@ function PageSection(props) {
             color: textColor,
             padding: theme.spacing(3),
         },
+
+        container: {
+            margin: theme.spacing(3)
+        }
     }));
 
     const classes = useStyle();
+    const cards = [1, 2, 3, 4, 5, 6];
 
     return (
         <ThemeProvider theme={theme}>
             <Container {...others} classes={classes}>
                 <Typography variant="h3" align={headingAlignment}>{heading}</Typography>
                 <Typography variant="h5" align={headingAlignment}>{description}</Typography>
-                <EventCard
-                    cardHeading = "Bubba Grump Shrimp Co."
-                    cardDescription = "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                    across all continents except Antarctica"
-                    cardImage = "http://source.unsplash.com/collection/146130/random"
-                />
+                <Grid container spacing={2} className={classes.container}>
+                        {cards.map(() => 
+                            <Grid item>
+                                <EventCard
+                                    cardHeading = "Bubba Grump Shrimp Co."
+                                    cardDescription = "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica"
+                                    cardImage = "http://source.unsplash.com/collection/146130/random"
+                                />
+                            </Grid>
+                        )}
+                </Grid>
             </Container>
         </ThemeProvider>
     );
