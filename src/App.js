1dad5c6b41f517241d99260b016818c34d190338
malpani-keshav <keshavmalpani01@gmail.com>
2019-12-16 23:53:24 +0530

    Category Speed Dial Added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ImageBanner from './ImageBanner';
 import PageSection from './PageSection';
 import EventCardRow from './EventCardRow'
 import SimpleGrid from './SimpleGrid';
+import CategorySpeedDial from './CategorySpeedDial'
 
 const theme = createMuiTheme({
   palette: {
@@ -24,7 +25,26 @@ const mainFeaturedPost = {
   linkText: 'Continue Reading...'
 };
 
-
+const pageSectionNamesWithIds=[
+  {
+    name:"Workshops",
+    id:"1"
+  },
+  {
+    name:"Competitions",
+    id:"2"
+  },
+  {
+    name:"Lectures",
+    id:"3"
+  },
+  {
+    name:"More",
+    id:"4"
+  },
+    
+    
+]
 function App() {
   return (
         <ThemeProvider theme={theme}>
@@ -37,6 +57,7 @@ function App() {
               containerBackgroundColor="white"
               textColor="black"
               description="This is the first event category at tryst. Lorem ipsum dolor sit amet. "
+              id="1"
             >
                 <EventCardRow>
                     <SimpleGrid n="3"/>
@@ -47,10 +68,64 @@ function App() {
                 <EventCardRow>
                     <SimpleGrid n="3"/>
                 </EventCardRow>
+            </PageSection>
+            <PageSection 
+              heading="Event Section 2"
+              headingAlignment="center"
+              containerBackgroundColor="white"
+              textColor="black"
+              description="This is the first event category at tryst. Lorem ipsum dolor sit amet. "
+              id="2"
+            >
                 <EventCardRow>
-                    <SimpleGrid n="4"/>
+                    <SimpleGrid n="3"/>
+                </EventCardRow>
+                <EventCardRow>
+                    <SimpleGrid n="2"/>
+                </EventCardRow>
+                <EventCardRow>
+                    <SimpleGrid n="3"/>
                 </EventCardRow>
             </PageSection>
+            <PageSection 
+              heading="Event Section 3"
+              headingAlignment="center"
+              containerBackgroundColor="white"
+              textColor="black"
+              description="This is the first event category at tryst. Lorem ipsum dolor sit amet. "
+              id="3"
+            >
+                <EventCardRow>
+                    <SimpleGrid n="3"/>
+                </EventCardRow>
+                <EventCardRow>
+                    <SimpleGrid n="2"/>
+                </EventCardRow>
+                <EventCardRow>
+                    <SimpleGrid n="3"/>
+                </EventCardRow>
+            </PageSection>
+            <PageSection 
+              heading="Event Section 4"
+              headingAlignment="center"
+              containerBackgroundColor="white"
+              textColor="black"
+              description="This is the first event category at tryst. Lorem ipsum dolor sit amet. "
+              id="4"
+            >
+                <EventCardRow>
+                    <SimpleGrid n="3"/>
+                </EventCardRow>
+                <EventCardRow>
+                    <SimpleGrid n="2"/>
+                </EventCardRow>
+                <EventCardRow>
+                    <SimpleGrid n="3"/>
+                </EventCardRow>
+            </PageSection>
+            <div style={{position:"fixed", bottom:40,right:40}}>
+              <CategorySpeedDial actions={pageSectionNamesWithIds}/>
+            </div>
             {/* <PageSection 
               heading="Event Section 2"
               headingAlignment="center"
