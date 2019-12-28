74e2606754bb58314e6af526f42a36a0e907891b
Ananye Agarwal <ananayagarwal@gmail.com>
2019-12-28 17:10:37 +0530

    Decrease the timeout for link switching

--- src/EventCard.js
+++ src/EventCard.js
@@ -37,7 +37,7 @@ function EventCard(props) {
     const classes = useStyles();
 
     const handleClick = (event) => {
-        setTimeout(() => history.push(linkTo) , 700);
+        setTimeout(() => history.push(linkTo) , 600);
     } 
 
     return (
