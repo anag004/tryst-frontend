c0eb1398f29f906590c2a315e2d92d0c6968c862
malpani-keshav <keshavmalpani01@gmail.com>
2020-01-02 22:52:51 +0530

    background animation added to coming soon page

--- src/ComingSoon.js
+++ src/ComingSoon.js
@@ -1,9 +1,8 @@
-import React, { Fragment, useEffect } from 'react';
+import React, {useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
-import { Container, makeStyles, Fade, Dialog, DialogTitle, DialogContent, Tabs, Tab, Slide, Paper, Zoom, Grow, IconButton } from '@material-ui/core';
+import { Container, makeStyles, Fade, IconButton } from '@material-ui/core';
 import TopNavBar from './TopNavBar';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import ScrollToTop from './ScrollToTop';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import TwitterIcon from '@material-ui/icons/Twitter'
@@ -14,48 +13,54 @@ const theme = createMuiTheme({
         secondary: { main: '#4CAF50' }
     }
 });
+const useStyle = makeStyles((theme) => ({
+    root: {
+        position:"absolute",
+        top:0,
+        bottom:0,
+        right:0,
+        left:0,
+    },
+    background: {
+        position: "fixed",
+        width: "100%",
+        height: "100%",
+        zIndex: -1
+    },
+
+    container:{
+        margin:"auto",
+        marginTop:60,
+    },
+    text:{
+        color:"white",
+        
+    },
+    countdownTime :{
+        fontSize:40,
+        color:"white",
+        display:"inline",
+        marginRight:10,
+        marginLeft:10,
+        padding:8,
+        borderStyle:"groove",
+        borderWidth:"2px",
+        borderRadius:"10px",
+    },
+    countdownText :{
+        marginTop:20,
+        fontSize:28,
+        color:"white",
+        display:"inline",
+        padding:15,
+    },
+    
+    
+}));
 
 export default function Lodging(props) {
     const {heading, containerBackgroundColor,textColor, ...others} = props;
 
-    const useStyle = makeStyles((theme) => ({
-        root: {
-            background: 'linear-gradient(to right, #314755,#26a0da)',
-            position:"absolute",
-            top:0,
-            bottom:0,
-            right:0,
-            left:0,
-        },
-        container:{
-            margin:"auto",
-            marginTop:60,
-        },
-        text:{
-            color:"white",
-            
-        },
-        countdownTime :{
-            fontSize:40,
-            color:"white",
-            display:"inline",
-            marginRight:12,
-            marginLeft:12,
-            padding:10,
-            borderStyle:"groove",
-            borderWidth:"2px",
-            borderRadius:"10px",
-        },
-        countdownText :{
-            marginTop:20,
-            fontSize:28,
-            color:"white",
-            display:"inline",
-            padding:15,
-        },
-        
-        
-    }));
     var TrystDate=new Date(2020, 2, 7);
     var today=new Date();
 
@@ -74,18 +79,20 @@ export default function Lodging(props) {
         },[]
     )
     return (
-        <div className={classes.root}>
-            <ScrollToTop/>
+        <ThemeProvider theme={theme} >
+            <div id="element" className={classes.background}>
+            </div>
             <TopNavBar threshold={10}/>
+            <Container classes={classes}>
             <Fade in={true} timeout={1000}>
                 <Container className={classes.container} maxWidth="lg">
-                    <Typography variant="h1" align="center" className={classes.text}> TRYST 2020</Typography>
+                    <Typography variant="h1" align="center" className={classes.text}> TRYST '20</Typography>
                     <br/>
                     <Fade in={show} timeout={800}>
-                        <Typography variant="h3" align="center" className={classes.text}>We are coming soon!</Typography>
+                        <Typography variant="h3" align="center" className={classes.text}>Coming Soon!</Typography>
                     </Fade>
                     <br/>
-                    <Typography variant="h6" align="center" className={classes.text}>Our website is under construction, follow us for updates now!</Typography>
+                    <Typography variant="h6" align="center" className={classes.text}>Follow us for updates now!</Typography>
                     <br/>
                     <br/>
                     <div align="center">
@@ -94,27 +101,23 @@ export default function Lodging(props) {
                         <br/>
                         <div className={classes.countdownText}>Days</div><div className={classes.countdownText}>Hours</div><div className={classes.countdownText}>Min</div><div className={classes.countdownText}>Second</div>
                     </div>
-                    <Container style={{position:"fixed",bottom:0,width:"100%"}}>
+                    <Container style={{position:"fixed",bottom:0,left: -10,width:"100%"}}>
                         <Container align="center" >
-                            <IconButton>
+                            <Typography variant="h5" className={classes.text}>Get in touch!</Typography>
+                            <IconButton style={{color: "white"}}>
                                 <FacebookIcon style={{fontSize:40}} />
                             </IconButton>
-                            <IconButton>
+                            <IconButton style={{color: "white"}}>
                                 <InstagramIcon style={{fontSize:40}} />
                             </IconButton>
-                            <IconButton>
+                            <IconButton style={{color: "white"}}>
                                 <TwitterIcon style={{fontSize:40}} />
                             </IconButton>
                         </Container>
                     </Container>
                 </Container>
             </Fade>
-        </div>
+            </Container>
+        </ThemeProvider>
     );
 }
-/*{time%2?
-                        <Zoom in={true} timeout={800}>
-                            <Typography variant="h3" align="center" className={classes.text}>We are coming soon!</Typography>
-                        </Zoom>
-                        :<Typography variant="h3" align="center" className={classes.text}>We are coming soon!</Typography>
-                    }*/ 
