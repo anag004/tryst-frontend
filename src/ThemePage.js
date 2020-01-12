3dd09eb48f94b3fd9c0c73be63f5958d753c7b79
Ananye Agarwal <ananayagarwal@gmail.com>
2020-01-12 15:51:06 +0530

    Add timeline barebones a single line that animates from top to bottom

--- src/ThemePage.js
+++ src/ThemePage.js
@@ -2,6 +2,12 @@ import React from 'react';
 import NavBar from './TopNavBar';
 import ImageBanner from './ImageBanner';
 import LandingScreen from './themeComponents/LandingScreen';
+import SVGVerticalLine from './themeComponents/SVGVerticalLine';
+import * as ScrollMagic from "scrollmagic";
+import { TweenMax, TimelineMax, Linear } from "gsap";
+import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+
+ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
 
 const post = {
     category:"The Team",
@@ -12,11 +18,33 @@ const post = {
     imgText: 'main image description',
 };
 
-export default function ThemePage(props) {
-    return (
-        <>
-            <NavBar threshold={10}></NavBar>
-            <LandingScreen/>
-        </>
-    );
+class ThemePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.controller = new ScrollMagic.Controller();
+    }
+
+    componentDidMount() {
+        const tween = new TimelineMax()
+                    .add(TweenMax.to("#verticalLine", 1, {strokeDashoffset: "0px"}, Linear.easeNone));
+        
+        new ScrollMagic.Scene({
+            triggerElement: '#startLineScroll',
+            duration: "120%",
+        })
+        .setTween(tween)
+        .addTo(this.controller);
+    }
+
+    render() {
+        return (
+            <>
+                <NavBar threshold={10} backgroundColor="black"></NavBar>
+                <LandingScreen/>
+                <SVGVerticalLine/>
+            </>
+        );
+    }
 }
+
+export default ThemePage;
---
+++ src/themeComponents/SVGVerticalLine.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <>
+      <div id="startLineScroll"></div>
+      <div align="center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="4.724"
+          height="742.008"
+          version="1.1"
+          viewBox="0 0 1.25 196.323"
+        >
+          <path
+            id="verticalLine"
+            fill="none"
+            stroke="#000"
+            strokeDasharray="200px"
+            strokeDashoffset="200px"
+            strokeLinecap="butt"
+            strokeLinejoin="miter"
+            strokeMiterlimit="4"
+            strokeOpacity="1"
+            strokeWidth="1.25"
+            d="M100.647 3.728V200.05"
+            transform="translate(-100.022 -3.728)"
+          ></path>
+        </svg>
+      </div>
+    </>
+  );
+}
+
+export default Icon;
