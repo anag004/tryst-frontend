66fe8f0e78081d41031cca8ef7b7aeaf6ef53fdc
malpani-keshav <keshavmalpani01@gmail.com>
2020-01-25 18:18:52 +0530

    Event detail backend

--- src/EventDetail.js
+++ src/EventDetail.js
@@ -1,50 +1,29 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Typography from '@material-ui/core/Typography';
-import { Container, makeStyles, Fade, Dialog, DialogTitle, DialogContent, Tabs, Tab, Slide, Paper, Grid, Link } from '@material-ui/core';
+import { Container, makeStyles, Fade, Dialog, DialogTitle, DialogContent, Tabs, Tab, Slide, Paper, Link } from '@material-ui/core';
 import ImageBanner from './ImageBanner';
-import TopNavBar from './TopNavBar';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
-import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
-import Grow from '@material-ui/core/Grow';
-import ScrollToTop from './ScrollToTop';
 import axios from 'axios';
 
-const theme = createMuiTheme({
-    palette: {
-        primary: { main: '#2196F3' },
-        secondary: { main: '#4CAF50' }
-    }
-});
 
 export default function EventDetail(props) {
+    const {heading, containerBackgroundColor,textColor, ...others} = props;
+    const [value, setValue] = React.useState("description");
+    const [section,setSection] = React.useState("<h1></h1>");
+    const [data,setData]=React.useState([]);
 
-    const [value,setValue]=React.useState([]);
-    const useStyle = makeStyles((theme) => ({
-        root: {
-            padding:theme.spacing(3)
-        }, 
-
-        fab: {
-            margin: 0,
-            top: 'auto',
-            right: 20,
-            bottom: 20,
-            left: 'auto',
-            position: 'fixed',
-            zIndex: 2
-        }
-    }));
-
+    // const Transition = React.forwardRef(function Transition(props) {
+    //     return <Slide timeout={1000} direction="up" />;
+    // });
     useEffect(()=>{
         axios.get('https://backend2020.tryst-iitd.org/api/event/view/'+(props.match.params.id))
         .then(res=>{const data=res.data
             // console.log(data.data)
-            setValue([(data.data)])
+            setData([(data.data)])
             // console.log(value)
         });
       },[])
-    const checkDisabled=(deadline)=>{
+    
+      const checkDisabled=(deadline)=>{
         // console.log(deadline);
         const deadline_date=new Date(deadline);
         // console.log(deadline_date);
@@ -56,122 +35,194 @@ export default function EventDetail(props) {
             return true;
         }
     }
+    
     const Time=(time)=>{
         const date=new Date(time);
         // console.log(date);
         return date.toLocaleTimeString('en-US');
     }
-    const classes = useStyle();
+    
 
-    return (
-        <ThemeProvider theme={theme}>
-            <ScrollToTop/>
-                <TopNavBar threshold={10}/>
-                {value.map(post=>   
-                    <div>
-                        <Grow in={true}>
-                            <Fab variant="extended" className={classes.fab} size="medium" href={post.reg_link} disabled={checkDisabled(post.reg_deadline)}>
-                                <AddIcon fontSize="small"/>
-                                register
-                            </Fab>
-                        </Grow>
-                        <Fade in={true}  timeout={1000}>
-                            <ImageBanner post={{category:post.category_name,title:post.name,description:post.subheading,image:((post.photos)[1])[0],imgText:""}}/>
-                        </Fade>
-                        <Fade in={true} timeout={1000}>
-                            <Container classes={classes} maxWidth="sm">
-                                <Typography variant="h4">Description</Typography>
-                                <br/>
-                                <Typography variant="body1">
-                                    {post.description} 
-                                </Typography>
-                                <br></br>
-                                {(post.dtv).length!=0?
-                                    <Fragment>
-                                        <Typography variant="h4">Event Info.</Typography>
-                                        <Grid container>
-                                        {(post.dtv).map(info=>
-                                            <Grid item sm style={{marginTop:10,marginLeft:25,marginBottom:10}}>
-                                                <Typography variant="body">Type: {info.type} </Typography><br/>
-                                                <Typography variant="body">Date: {info.date} </Typography><br/>
-                                                <Typography variant="body">Time: {Time(info.start_time)} </Typography><br/>
-                                                <Typography variant="body">Venue: {info.venue} </Typography><br/>
-                                            </Grid>
-                                            
-                                        )}
-                                        </Grid>
-                                    </Fragment>
-                                :
-                                    <Fragment/>
-                                }
-                                {(post.rules).length!=0?<Fragment>
-                                    <Typography variant="h4">Rules</Typography>
-                                    <br></br>
-                                    <Typography variant="body1">
-                                        {post.rules}
-                                    </Typography>
+    // const post = {
+    //     category:"Tryst Event Category 1",
+    //     title: 'Events @Tryst',
+    //     description:
+    //       "We are hosting some amazing events this year. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis felis ante, feugiat id laoreet eget, accumsan et ipsum..",
+    //     image: 'https://source.unsplash.com/random',
+    //     imgText: 'main image description',
+    //     linkText: 'Continue Reading...',
+    //     postDescription:{
+    //         about:"We are hosting some amazing events this year",
+    //         details:"the event will be held a s folows on this date and this time",
+    //         prize:"The prizes for the event are as follows",
+    //         register:"Follow below to register"
+    //     },
+    // };
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+        // showData(post.description.{newValue})
+    };
+    
+    const useStyle = makeStyles (() => ({
+        root:{
+            scrollbarWidth:"none",
+            
+        },
+        dialog: {
+            backgroundImage: 'url(http://source.unsplash.com/collection/146130/random)',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+          },
+        imageBanner:{
+            height:420,
+            width:window.innerWidth>1000?"auto":"auto",
+            overflow:"hidden",
+            marginLeft:"auto"
+        },
+        data:{
+            margin:window.innerWidth>1000?20:10,
+            fontSize:window.innerWidth>1000?20:15
+        },
+        tabs:{
+            fontSize:window.innerWidth>1000?20:"auto",
+            marginTop:window.innerWidth>1000?20:"auto",
+            color:"black",  
+        },
+        category:{
+            fontSize:20,
+            backgroundColor:"#D5D3D6",
+        },
+        heading:{
+            marginTop:20
+        },
+        dialogContent:{
+            backgroundColor:"#D5D3D6",
+        }
+    }));
+    const getSection=(value,post)=>{
+        if(value=="description"){
+            return(
+                <div>
+                    <Typography variant="body1">
+                        {post.description} 
+                    </Typography>
+                </div>
+            )
+        }
+        else if(value=="rules"){
+            return(
+                <div>
+                    <Typography variant="h6">The following rules are binding:</Typography>
+                    <Typography variant="body1">
+                        <ol>
+                            {((post.rules).list).map(str=><li>{str}</li>)}
+                        </ol>
+                    </Typography>
+                </div>
+            )
+        }
+        else if(value=="prizes"){
+            return(
+                <Typography variant="body1">
+                    {post.prizes}
+                </Typography>
+            )
+        }
+        else if(value=="contact"){
+            return(
+                <Typography>
+                    <ul>
+                        {(post.poc).map(obj=>
+                            <li>
+                                <Fragment>
+                                    Name: {obj.name}
                                     <br/>
+                                    Email: {obj.email}
+                                    <br/>
+                                    Designation: {obj.designation}
+                                    <br/>   
                                 </Fragment>
-                                :
-                                    <Fragment/>
-                                }
-                                {/* <Typography variant="body1">
-                                    <ol>
-                                        {((post.rules).list).map(str=><li>{str}</li>)}
-                                    </ol>
-                                </Typography> */}
-                                {(post.prizes).length!=0?<Fragment>
-                                    <Typography variant="h4">Prizes</Typography>
-                                    <br></br>
-                                    <Typography variant="body1">
-                                        {post.prizes}
-                                    </Typography>
-                                </Fragment>
-                                :<Fragment/>
-                                }
-                                {/* <Typography variant="body1">
-                                    <ul>
-                                    {((post.prizes).list).map(str=><li>{str}</li>)}
-                                    </ul>
-                                </Typography> */}
-                                <br/>
-                                {(post.poc).length!=0?<Fragment>
-                                    <Typography variant="h4">
-                                        Contact Info
-                                    </Typography>
-                                    <Typography>
-                                        <ul>
-                                            {(post.poc).map(obj=>
-                                                <Fragment>
-                                                    Name: {obj.name}
-                                                    <br/>
-                                                    Email: {obj.email}
-                                                    <br/>
-                                                    Designation: {obj.designation}
-                                                    <br/>   
-                                                </Fragment>
-                                            )}
-                                        </ul>
-                                    </Typography>
-                                </Fragment>
-                                :<Fragment/>
-                                }
-                                {(post.url).length!=0?
-                                    <Fragment>
-                                        <Link href={"http://"+(post.url)} target="_blank" rel="noreferrer">
-                                            <Typography variant="h6">Problem Statement</Typography>
-                                        </Link>
-                                    </Fragment>
-                                :
-                                    <Fragment/>
-                                }
-                                <br/>
-                                <Typography variant="h5">Organised by: {post.category_name} </Typography>
-                                <br/>
+                            </li>
+                        )}
+                    </ul>
+                </Typography>
+            )
+        }
+        else if(value=="register"){
+            return(
+                <Link to={post.reg_link}>
+                    <Typography variant="h6">Click here to register!</Typography>
+                </Link>
+            )
+        }
+        else{
+            return(
+                <div>
+                {(post.dtv).map(info=>(
+                    <div>
+                        {value==info.type?
+                            <ul>
+                                <li><Typography variant="body">Date: {info.date} </Typography></li>
+                                <li><Typography variant="body">Time: {Time(info.start_time)} </Typography></li>
+                                <li><Typography variant="body">Venue: {info.venue} </Typography></li>
+                            </ul>
+                        :
+                        null}
+                    </div>
+                ))}
+                </div>
+            )
+        }
+    }
+    const classes=useStyle()
+    var z;
+    return (
+        <div className={classes.root}>
+            {data.map(post=>
+                <Dialog
+                    open={true}
+                    maxWidth="false"
+                    className={classes.dialog}
+                    // TransitionComponent={Transition}
+                >
+                    <DialogTitle className={classes.category}>
+                        Organised by: {post.category_name}
+                    </DialogTitle>
+                    {/* <Paper className={classes.paper}> */}
+                        <DialogContent className={classes.dialogContent}>
+                            <div className={classes.imageBanner}>
+                                <ImageBanner post={{category:post.category_name,title:post.name,description:post.subheading,image:"https://source.unsplash.com/random",imgText:""}}/>
+                            </div>
+                            <Typography variant="h3" className={classes.heading} > {post.name} </Typography>
+                            {(post.url).length!=0?<Link href={"http://"+(post.url)} target="_blank" rel="noreferrer"><Typography variant="h6" className={classes.heading} >Problem Statement</Typography></Link>:null}
+                            <Tabs
+                                value={value}
+                                onChange={handleChange}
+                                indicatorColor="primary"
+                                textColor="primary"
+                                variant="scrollable"
+                                scrollButtons="auto"
+                                // className={classes.tabs}    
+                            >
+                                {(post.description).length!=0?<Tab label="About" value="description" className={classes.tabs} />:null}
+                                {(post.rules).length!=0?<Tab label="Rules" value="rules" className={classes.tabs} />:null}
+                                {(post.prizes).length!=0?<Tab label="Prizes" value="prizes" className={classes.tabs} />:null}
+                                <Tab label="Register" value="register" className={classes.tabs} disabled={checkDisabled(post.reg_deadline)} />
                                 
-                            </Container>
-                        </Fade>
-                    </div>)}
-        </ThemeProvider>
+                                {(post.poc).length!=0?<Tab label="Contact Info." value="contact" className={classes.tabs} />:null}
+                                {(post.dtv).map(info=>(
+                                    <Tab label={info.type} value={info.type} className={classes.tabs}/>
+                                ))}
+                            </Tabs>
+                            <div className={classes.data}>
+                                {getSection(value,post)}
+                            </div>
+                        </DialogContent>
+                    {/* </Paper> */}
+                </Dialog>
+            )}
+        </div>
     );
 }
