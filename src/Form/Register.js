1aaf8a073072ba7ff42fd93adc40f34b491c4b69
Ananye Agarwal <ananayagarwal@gmail.com>
2020-01-26 15:02:36 +0530

    Add a simple team register form

--- src/Form/Register.js
+++ src/Form/Register.js
@@ -2,7 +2,7 @@ import React from 'react';
 import NavBar from '../TopNavBar';
 import TextField from '@material-ui/core/TextField';
 import Card from '@material-ui/core/Card';
-import { withStyles } from "@material-ui/core/styles";
+import { makeStyles } from "@material-ui/core/styles";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
@@ -10,55 +10,72 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
+import AddIcon from '@material-ui/icons/Add';
+import TeamRegisterCard from './TeamRegisterCard';
+import IconButton from '@material-ui/core/IconButton';
+import Fade from '@material-ui/core/Fade';
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
     card: {
         padding: theme.spacing(4), 
-        align: "center"
+        align: "center",
+        marginTop: theme.spacing(3)
     },
 
     container: {
-        marginTop: theme.spacing(3),
         maxWidth: 800,
     },
 
     textfield: {
         margin: theme.spacing(3)
-    }
-})
+    },
 
-class Register extends React.Component {
-    render() {
-        const handleChange = (event) => {
-            console.log(event.target.value);
-        }
-
-        return (
-            <>
-                <NavBar threshold={10} disableOpacity={true} backgroundColor="black"/>
-                <Typography variant="h2" style={{marginTop: 100}} align="center">Registration</Typography>
-                    <RadioGroup row onChange={handleChange} defaultValue="Individual">
-                        <Container className={this.props.classes.container}>
-                            <FormControlLabel value="Individual" control={<Radio />} label="Individual" />
-                            <FormControlLabel value="Team" control={<Radio />} label="Team" />
-                        </Container>
-                    </RadioGroup>
-                <Container className={this.props.classes.container}>
-                    <Card className={this.props.classes.card}>
-                        <Typography variant="h4">Team Member</Typography>
-                        <Grid container>
-                            <Grid item xs={6}>
-                                <TextField className={this.props.classes.textfield} label="Name" variant="outlined" required="true"/>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <TextField className={this.props.classes.textfield} label="Email ID" variant="outlined" required="true"/>
-                            </Grid>
-                        </Grid>
-                    </Card>
-                </Container>
-            </>
-        )
+    addButton: {
+        left: "50%",
+        marginLeft: -17,
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+        position: "relative"
     }
+}));
+
+function Register(props) {
+    const classes = useStyles();
+    const [size, setSize] = React.useState(2);
+    const [value, setValue] = React.useState("Individual")
+
+    const handleChange = (event) => {
+        console.log(event.target.value);
+        setValue(event.target.value);
+    }
+
+    const handleClick = () => {
+        setSize(size + 1);
+    }
+
+    const teamNumbers = [];
+    for(let i = 1; i <= size; i++) {
+        teamNumbers.push(i);
+    }
+
+    return (
+        <>
+            <NavBar threshold={10} disableOpacity={true} backgroundColor="black"/>
+            <Typography variant="h2" style={{marginTop: 100}} align="center">Registration</Typography>
+                <RadioGroup row onChange={handleChange} defaultValue="Individual">
+                    <Container className={classes.container}>
+                        <FormControlLabel value="Individual" control={<Radio />} label="Individual" />
+                        <FormControlLabel value="Team" control={<Radio />} label="Team" />
+                    </Container>
+                </RadioGroup>
+            <Container className={classes.container}>
+                {teamNumbers.map((number) => <TeamRegisterCard number={number}/>)}
+                <IconButton variant="extended" color="primary" className={classes.addButton} onClick={handleClick}>
+                    <AddIcon/>
+                </IconButton>
+            </Container>
+        </>
+    )
 }
 
-export default withStyles(styles)(Register);
+export default Register;
---
+++ src/Form/TeamRegisterCard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from "@material-ui/core/styles";
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+
+const useStyles = makeStyles((theme) => ({
+    card: {
+        padding: theme.spacing(4), 
+        align: "center",
+        marginTop: theme.spacing(3)
+    },
+
+    textfield: {
+        margin: theme.spacing(3)
+    }
+}));
+
+function TeamRegisterCard(props) {
+    const {number, ...other} = props;
+    const classes = useStyles();
+
+    return (
+        <Card className={classes.card}>
+            <Typography variant="h4">Team Member #{number}</Typography>
+            <Grid container>
+                <Grid item xs={6}>
+                    <TextField className={classes.textfield} label="Name" variant="outlined" required="true"/>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField className={classes.textfield} label="Email ID" variant="outlined" required="true"/>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField className={classes.textfield} label="Contact No." variant="outlined" required="true"/>
+                </Grid>
+                <Grid item xs={6}>
+                    <TextField className={classes.textfield} label="College" variant="outlined" required="true"/>
+                </Grid>
+            </Grid>
+        </Card>
+    )
+}
+
+export default TeamRegisterCard;
