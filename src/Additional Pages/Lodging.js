d792fe876cbc0b606592ecd73da81d73bd32c8fe
malpani-keshav <keshavmalpani01@gmail.com>
2020-02-19 16:53:35 +0530

    lodging page

--- src/Additional Pages/Lodging.js
+++ src/Additional Pages/Lodging.js
@@ -2,10 +2,10 @@ import React, { Fragment } from 'react';
 import Typography from '@material-ui/core/Typography';
 import { Container, makeStyles, Fade, Dialog, DialogTitle, DialogContent, Tabs, Tab, Slide, Paper } from '@material-ui/core';
 import ImageBanner from '../ImageBanner';
-import TopNavBar from '../TopNavBar';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import ScrollToTop from '../ScrollToTop';
 import { MetaTags } from 'react-meta-tags';
+import NavBar from '../TopNavBar';
 
 const theme = createMuiTheme({
     palette: {
@@ -40,19 +40,20 @@ export default function Lodging(props) {
                 <meta name="title_Page" content="TRYST 2020, IIT Delhi, Lodgings" />
             </MetaTags>
             <ScrollToTop/>
-            <TopNavBar threshold={10}/>
+            <NavBar threshold={10} backgroundColor="#192841"/>
+            <div style={{position:"fixed",width:"100%",height:"100%",zIndex:"-1",backgroundColor:"#29426c"}} />
             <Fade in={true}  timeout={1000}>
                 <ImageBanner post={post}/>
             </Fade>
             <Fade in={true} timeout={1000}>
                 <Container classes={classes} maxWidth="sm">
-                    <Typography variant="h4" align="center">Affordable. Convenient. Unique</Typography>
+                    <Typography variant="h4" style={{fontFamily:['Montserrat','sans-serif'].join(','), color:"white"}} align="center">Affordable. Convenient. Unique</Typography>
                     <br/>
-                    <Typography variant="body1">
-                        Stay in one of the 13 hostels at IITD and experience IITD's unique hostel culture. Never be more than 5 minutes away from the action. All for a fraction of the cost of a hotel. Etiam bibendum, augue eu ultrices ullamcorper, lacus tortor condimentum justo, eget vestibulum nulla magna egestas massa. Maecenas sapien lacus, placerat in ipsum vel, ultricies dapibus mi. Fusce sagittis turpis quis justo lacinia pulvinar. Duis porta mollis turpis at commodo. Nunc imperdiet cursus nibh, at feugiat turpis mollis at. Maecenas venenatis risus id ex scelerisque iaculis. Integer sed arcu ultrices, gravida tellus eget, rhoncus tortor. Donec condimentum quis lectus nec fringilla. Proin viverra mauris at tellus molestie lacinia. 
+                    <Typography variant="body1" style={{fontFamily:['Montserrat','sans-serif'].join(','), color:"white"}}>
+                        Stay in one of the 13 hostels at IITD and experience IITD's unique hostel culture. Never be more than 5 minutes away from the action. All for a fraction of the cost of a hotel. 
                     </Typography>
                     <br></br>
-                    <Typography variant="h4">Eligibility</Typography>
+                    {/* <Typography variant="h4">Eligibility</Typography>
                     <br></br>
                     You must satisfy the following criteria to be eligible for a reservation
                     <Typography variant="body1">
@@ -62,22 +63,23 @@ export default function Lodging(props) {
                             <li>We do <i>not</i> talk about Fight Club. Maecenas sapien lacus, placerat in ipsum vel, ultricies dapibus mi. </li>
                             <li>We do <u>not</u> talk about Fight Club.  Fusce sagittis turpis quis justo lacinia pulvinar. Duis porta mollis turpis at commodo</li>
                         </ol>
-                    </Typography>
-                    <Typography variant="h4">Rates</Typography>
+                    </Typography> */}
+                    <Typography variant="h4" style={{fontFamily:['Montserrat','sans-serif'].join(','), fontSize:"40px", color:"white"}}>Rates</Typography>
                     <br></br>
-                    Dirt cheap rates
-                    <Typography variant="body1">
+                    <Typography variant="body" style={{fontFamily:['Montserrat','sans-serif'].join(','), color:"white"}}>Dirt cheap rates</Typography>
+                    <Typography variant="body1" style={{fontFamily:['Montserrat','sans-serif'].join(','), color:"white"}}>
                         <ul>
-                            <li><i>One night: </i> 2000 INR</li>
-                            <li><i>Two nights: </i> 3000 INR</li>
-                            <li><i>Three nights: </i> 5000 INR</li>
+                            <li><i>Day One </i> 500 INR</li>
+                            <li><i>Day Two: </i> 1000 INR</li>
+                            <li><i>Day Three: </i> 1000 INR</li>
+                            <li><i>Total for 3D 2N: </i> 2500 INR</li>
                         </ul>
                     </Typography>
-                    <Typography variant="body2">
+                    {/* <Typography variant="body2">
                         <b> 
                             Contact urnasemper@gmail.com to make resevations.
                         </b>
-                    </Typography>
+                    </Typography> */}
                 </Container>
             </Fade>
             
