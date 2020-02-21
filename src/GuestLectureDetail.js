f3a45f2a0f86472f15952468f7d377a5fe5da9c5
malpani-keshav <keshavmalpani01@gmail.com>
2020-02-22 01:30:01 +0530

    register disable/enable guest lecture

--- src/GuestLectureDetail.js
+++ src/GuestLectureDetail.js
@@ -44,20 +44,34 @@ export default function GuestLectureDetail(props){
 
     // const {left_side,backgroundColor}=props;
     const [value,setValue]=React.useState([]);
+    const [data,setData]=React.useState([]);
     useEffect(()=>{
         axios.get('https://backend2020.tryst-iitd.org/api/event/view/'+(props.match.params.id))
         .then(res=>{const data=res.data
-            console.log(data.data)
-            setValue((data.data.photos))
+            // console.log(data.data)
+            setData(data.data);
+            setValue((data.data.photos));
             id=data.data.id;
-            console.log(id)
-            console.log("dbsafjbazvbzvhk")
+            // console.log(id)
+            // console.log("dbsafjbazvbzvhk")
             // console.log(value)
         });
       },[])
     var left_side=true;
     const classes=useStyles();
     const largeScreen = useMediaQuery('(min-width:500px)');
+    const checkDisabled=(deadline)=>{
+        // console.log(deadline);
+        const deadline_date=new Date(deadline);
+        // console.log(deadline_date);
+        const today=new Date();
+        if(today.getTime()<deadline_date.getTime()){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
     return(
         <ThemeProvider theme={theme}>
             <MetaTags>
@@ -75,9 +89,10 @@ export default function GuestLectureDetail(props){
                             </>
                         )}
                     </div>
-                    <div style={{position:"fixed", bottom:40,right:largeScreen?40:20}}>
+                    {data.reg_status?(!checkDisabled(data.reg_deadline)?<div style={{position:"fixed", bottom:40,right:largeScreen?40:20}}>
                         <Button variant="contained" color="secondary" href={"/register/"+props.match.params.id} size="large">Register</Button>
-                    </div>
+                    </div>:null):null}
+                    {/* {console.log(data)} */}
                 </div>
             </div>
         </ThemeProvider>
