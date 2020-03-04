0b886f637c4ad2c688b21a284a956c60897f91b8
Ananye Agarwal <ananayagarwal@gmail.com>
2020-03-04 18:44:03 +0530

    Add postpone notification

--- src/themeComponents/LandingScreen.js
+++ src/themeComponents/LandingScreen.js
@@ -86,6 +86,15 @@ const styles = theme => ({
     },
     tooltip: {
         marginTop: theme.spacing(10)
+    },
+    notif: {
+        display: "inline-block",
+        width: "70%",
+        left: "10%",
+        padding: theme.spacing(3),
+        position: "absolute",
+        zIndex: 4,
+        backgroundColor: "rgba(255, 255, 255, 0.8)",
     }
 });
 
@@ -123,6 +132,13 @@ class LandingScreen extends React.Component {
                         </Typography>
                     </Fade>
                 </Grid>
+
+            
+                <Paper className={this.props.classes.notif}>
+                    <Typography variant="h4">Tryst postponed</Typography>
+                    <Typography variant="body1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</Typography>
+                </Paper>
+  
                 
                 <Grid container md={12} style={{position:"relative",height:"150px"}} justify="center">
                         <Fade in={true} timeout={4000}>
